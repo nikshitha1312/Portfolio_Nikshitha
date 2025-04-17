1e887eb2e325e89ca0621b67bf92270a99c1ed96
nikshitha1312 <132698737+nikshitha1312@users.noreply.github.com>
2025-04-17 11:40:46 +0530

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -11,18 +11,21 @@ function Projects() {
         <p>The Endangered Species Detection and Prevention project uses machine learning to identify endangered species from images and videos, analyze population trends, and explore conservation measures.Trained and Tested 12000+ images.</p>
         <p>Technologies Used: Python, Jupyter Notebook, C++, HTML, CNN,Yolo</p>
         <p>Key Achievement: Implemented a CNN model achieving 85% accuracy in detecting endangered species across 10,000+ images. </p>
+    <p><a href="https://github.com/nikshitha1312/Endangered_Species" target="_blank">View Project on GitHub</a></p>
       </div>
       <div className={styles.project}>
         <h3>My Portfolio Website</h3>
         <p>Built a personal portfolio website to showcase education, projects, contact details, and achievements. </p>
         <p>Technologies Used: HTML, JavaScript, React, CSS</p>
         <p>Key Achievement: Designed a responsive and clean UI using React and deployed it for public access. </p>
+    <p><a href="https://github.com/nikshitha1312/resume-website-project" target="_blank">View Project on GitHub</a></p>
       </div>
       <div className={styles.project}>
-        <h3>Travel Guide</h3>
+        <h3>TripMate</h3>
         <p>Implemented a Travel Manager to handle up to 100+ user submissions with a focus on accuracy and reliability. </p>
         <p>Technologies Used: React, HTML, JavaScript, CSS</p>
         <p>Key Achievement: Showcased 4 key sites in Japan, offering insights on 3+ areas with video links attached. </p>
+    <p><a href="https://github.com/nikshitha1312/Trip_Mate" target="_blank">View Project on GitHub</a></p>
       </div>
     </section>
   );
