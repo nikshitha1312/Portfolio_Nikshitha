cbe415270ebd3883625e802b0d7cdd5fcf36c339
nikshitha1312 <132698737+nikshitha1312@users.noreply.github.com>
2025-04-28 10:44:45 +0530

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -11,14 +11,14 @@ function Projects() {
         <p>The Endangered Species Detection and Prevention project uses machine learning to identify endangered species from images and videos, analyze population trends, and explore conservation measures.Trained and Tested 12000+ images.</p>
         <p>Technologies Used: Python, Jupyter Notebook, C++, HTML, CNN,Yolo</p>
         <p>Key Achievement: Implemented a CNN model achieving 85% accuracy in detecting endangered species across 10,000+ images. </p>
-    <p><a href="https://github.com/nikshitha1312/Endangered_Species" target="_blank">View Project on GitHub</a></p>
+    <p><a href="https://github.com/nikshitha1312/Endagered_species_guide" target="_blank">View Project on GitHub</a></p>
       </div>
       <div className={styles.project}>
         <h3>My Portfolio Website</h3>
         <p>Built a personal portfolio website to showcase education, projects, contact details, and achievements. </p>
         <p>Technologies Used: HTML, JavaScript, React, CSS</p>
         <p>Key Achievement: Designed a responsive and clean UI using React and deployed it for public access. </p>
-    <p><a href="https://github.com/nikshitha1312/resume-website-project" target="_blank">View Project on GitHub</a></p>
+    <p><a href="https://github.com/nikshitha1312/Portfolio_Nikshitha" target="_blank">View Project on GitHub</a></p>
       </div>
       <div className={styles.project}>
         <h3>TripMate</h3>
@@ -26,6 +26,24 @@ function Projects() {
         <p>Technologies Used: React, HTML, JavaScript, CSS</p>
         <p>Key Achievement: Showcased 4 key sites in Japan, offering insights on 3+ areas with video links attached. </p>
     <p><a href="https://github.com/nikshitha1312/Trip_Mate" target="_blank">View Project on GitHub</a></p>
+      </div>
+     <div className={styles.project}>
+        <h3>workspace</h3>
+        <p>This is a modern React application built with Next.js, TypeScript, and Tailwind CSS. It features a responsive three-pane layout with interactive components and smooth animations.Features
+Responsive three-pane layout with animated transitionsModern UI components built with Tailwind CSSType-safe development with TypeScriptInteractive state management Reusable component library </p>
+        <p>The application is built with:
+Next.js 13+ with App Router
+TypeScript for type safety
+Tailwind CSS for styling
+Framer Motion for animations
+Class Variance Authority for component variants</p>
+        <p>Component-based architecture
+Type-safe development
+Responsive design
+Reusable components
+Clean and maintainable code structure
+Modern React patterns and hooks </p>
+    <p><a href="https://github.com/nikshitha1312/workspace" target="_blank">View Project on GitHub</a></p>
       </div>
     </section>
   );
